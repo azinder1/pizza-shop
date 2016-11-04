780be6e9eb2e119e34ba6ec0980517a7892925ed
Adam Zinder <adam.zinder@gmail.com>
2016-11-04 06:09:33 -0700

    provide specs in README

---
+++ README.md
@@ -0,0 +1,69 @@
+# _PIZZA SHOP_
+
+#### _Pizza ordering site with cost calculator, November 4th, 2016_
+
+#### By _**Adam Zinder**_
+
+## Description
+
+_Can take in user preferences for pizza order and can output cost_
+
+## Setup/Installation Requirements
+
+* _https://azinder1.github.io/pizza-shop/_
+
+## Known Bugs
+
+_None_
+
+## Support and contact details
+
+_adam.zinder@gmail.com_
+
+## Technologies Used
+
+_HTML, JavaScript, jQuery, Bootstrap_
+
+### License
+
+*MIT*
+
+Copyright (c) 2016 **_Adam Zinder_**
+
+## Behavioral Specs
+
+**The program should take in basic user preferences*
+
+  _Input: Are you a vegetarian, yes or no?_
+
+  _Output: alert("yes")_
+
+**The program should take in user pizza selections*  
+
+  _Input: Ingredients: Pepperoni, onions  _
+
+  _Output: alert("pepperoni, onions")_
+
+**The program should take inputted preferences and store them in a constructor*
+
+  _Input: pepperoni, onions_
+
+  _Return: receipt.ingredients = pepperoni, onions_
+
+**The program should have a prototype that calculates cost based on user preference*
+
+_Input: 2 ingredients, medium_
+
+_Output: $10.00_
+
+**The program should show all user preferences and calculated cost*
+
+_Input: pepperoni, onions, medium_
+
+_Output: "You purchased a medium pepperoni and onion pizza and it will cost $10.00_
+
+**The program should hide and show different elements and should be styled*
+
+_Input: Ingredients_
+
+_Output: Hide form and show final receipt_
--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
-function Receipt(preference, ingredients, pizzaSize, price) {
+function Receipt(ingredients, pizzaSize) {
   this.ingredients = ingredients;
   this.pizzaSize = pizzaSize;
-  this.price = price;
+  //this.price = price;
 }
 
 var vegIngredients = ['tomatoes', 'onions', 'mushrooms', 'olives', 'peppers', 'artichoke']
 var vegAndMeatIngredients = ['chicken', 'sausage', 'pepperoni', 'tomatoes', 'onions', 'mushrooms', 'olives', 'peppers', 'artichoke']
+
 
 $(document).ready(function(){
   $('form#preferenceForm').submit(function(event){
@@ -13,14 +14,25 @@ $(document).ready(function(){
     var preference = $("input:radio[name=preference]:checked").val();
     if (preference === "yes") {
       vegIngredients.forEach(function(vegIngredient){
-        $('.pizzaIngredients').append('<input type=\"checkbox\" name=\"ingredients\" value=\"'+ vegIngredient + '\">' + vegIngredient +'<br>')
+        $('.pizzaIngredients p').append('<input type=\"checkbox\" name=\"ingredients\" value=\"'+ vegIngredient + '\">' + vegIngredient +'<br>')
       });
     }
     else {
       vegAndMeatIngredients.forEach(function(vegAndMeatIngredient){
-        $('.pizzaIngredients').append('<input type=\"checkbox\" name=\"ingredients\" value=\"'+ vegAndMeatIngredient + '\">' + vegAndMeatIngredient +'<br>')
+        $('.pizzaIngredients p').append('<input type=\"checkbox\" name=\"ingredients\" value=\"'+ vegAndMeatIngredient + '\">' + vegAndMeatIngredient +'<br>')
       });
     }
-
+  });
+  $('form#pizzaForm').submit(function(event){
+    event.preventDefault();
+    var inputtedIngredients = [];
+    $("input:checkbox[name=ingredients]:checked").each(function(){
+      var eachIngredient = $(this).val();
+      inputtedIngredients.push(eachIngredient);
+    });
+    var inputtedSize =  $("#pizzaSizeOption").val();
+    console.log(inputtedSize);
+    console.log(inputtedIngredients);
+    var pizzaOrder = new Receipt (inputtedSize, InputtedIngredients)
   });
 });
